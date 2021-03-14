63c27c705ca3692b5a47e26671e06c01805814f0
wangpeilei <o6996o@163.com>
2021-03-14 17:09:46 +0800

    hi-test

---
+++ src/pool/TestA.java
@@ -0,0 +1,11 @@
+package pool;
+
+/**
+ * @author: wangpeilei
+ * @date: 2021/03/14 17:08
+ **/
+public class TestA {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
